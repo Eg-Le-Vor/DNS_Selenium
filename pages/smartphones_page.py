cd7a38b6bb47efe8bcca9be5aebe005ad37f31bf
unknown <at-for-job@yandex.ru>
2023-03-08 13:14:52 +0300

    Select smartphone 2

--- pages/smartphones_page.py
+++ pages/smartphones_page.py
@@ -11,17 +11,19 @@ MIN_PRICE_FIELD_LOCATOR = "//input[@class='ui-input-small__input ui-input-small_
 MAX_PRICE_FIELD_LOCATOR = "//input[@class='ui-input-small__input ui-input-small__input_list'][@placeholder='до 169 299']"
 DISCOUNTS_CHECKBOX_BUTTON_LOCATOR = "//label[@class='ui-checkbox ui-checkbox_list']//input[@value='tovarysoskidkoj']//following-sibling::span"
 XIAOMI_CHECKBOX_BUTTON_LOCATOR = "//label[@class='ui-checkbox ui-checkbox_list']//input[@value='xiaomi']//following-sibling::span"
+REALME_CHECKBOX_BUTTON_LOCATOR = "//label[@class='ui-checkbox ui-checkbox_list']//input[@value='realme']//following-sibling::span"
 RELIABLE_MODELS_CHECKBOX_BUTTON_LOCATOR = "//div[@data-id='rely']//label[@class='ui-checkbox']//input[@type='checkbox']//following-sibling::span"
 REVIEW_CHECKBOX_BUTTON_LOCATOR = "//div[@data-id='review']//label[@class='ui-checkbox']//input[@type='checkbox']//following-sibling::span"
 APPLY_FILTERS_BUTTON_LOCATOR = "//button[@data-role='filters-submit']"
 SORTING_METHOD_BUTTON_LOCATOR = "//div[@data-id='order']/a[@class='ui-link ui-link_blue']/span[@class='top-filter__selected']"
 POPULAR_CHECKBOX_BUTTON_LOCATOR = "//label[@class='ui-radio__item']/span[contains(text(), 'Сначала популярные')]"
+BEST_SCORE_CHECKBOX_BUTTON_LOCATOR = "//label[@class='ui-radio__item']/span[contains(text(), 'Сначала с лучшей оценкой')]"
 FIRST_SMARTPHONE_NAME_LOCATOR = "/html/body/div[2]/div/div[2]/div[2]/div[3]/div/div[2]/div[1]/a/span"
 FIRST_SMARTPHONE_PRICE_LOCATOR = "/html/body/div[2]/div/div[2]/div[2]/div[3]/div/div[2]/div[1]/div[4]/div/div[1]"
 FIRST_SMARTPHONE_BUY_BUTTON_LOCATOR = "/html/body/div[2]/div/div[2]/div[2]/div[3]/div/div[2]/div[1]/div[4]/button[2]"
 TO_CART_BUTTON_LOCATOR = "/html/body/div[1]/header/div[1]/nav/div/div[3]/div[1]/div/div/div/div[2]/div[2]/button[2]/span"
 
-MIN_PRICE_1 = "5000"
+MIN_PRICE_1 = "10000"
 MAX_PRICE_1 = "50000"
 
 
@@ -46,6 +48,9 @@ class Smartphones_page(Base):
     def get_xiaomi_checkbox_button(self):
         return WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, XIAOMI_CHECKBOX_BUTTON_LOCATOR)))
     
+    def get_realme_checkbox_button(self):
+        return WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, REALME_CHECKBOX_BUTTON_LOCATOR)))
+    
     def get_reliable_models_checkbox_button(self):
         return WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, RELIABLE_MODELS_CHECKBOX_BUTTON_LOCATOR)))
     
@@ -60,6 +65,9 @@ class Smartphones_page(Base):
     
     def get_popular_checkbox_button(self):
         return WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, POPULAR_CHECKBOX_BUTTON_LOCATOR)))
+    
+    def get_best_score_checkbox_button(self):
+        return WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, BEST_SCORE_CHECKBOX_BUTTON_LOCATOR)))
     
     def get_first_smartphone_name(self):
         return WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, FIRST_SMARTPHONE_NAME_LOCATOR)))
@@ -99,6 +107,12 @@ class Smartphones_page(Base):
         self.move_to_element(xiaomi_checkbox_button, self.driver)
         xiaomi_checkbox_button.click()
         print('Выбран производитель "Xiaomi".')
+
+    def click_realme_checkbox_button(self):
+        realme_checkbox_button = self.get_realme_checkbox_button()
+        self.move_to_element(realme_checkbox_button, self.driver)
+        realme_checkbox_button.click()
+        print('Выбран производитель "realme".')
     
     def click_reliable_models_checkbox_button(self):
         reliable_models_checkbox_button = self.get_reliable_models_checkbox_button()
@@ -130,6 +144,12 @@ class Smartphones_page(Base):
         popular_checkbox_button.click()
         print('Выбран метод сортировки "Сначала популярные".')
 
+    def click_best_score_checkbox_button(self):
+        best_score_checkbox_button = self.get_best_score_checkbox_button()
+        self.move_to_element(best_score_checkbox_button, self.driver)
+        best_score_checkbox_button.click()
+        print('Выбран метод сортировки "Сначала с лучшей оценкой".')
+
     def click_first_smartphone_buy_button(self):
         first_smartphone_buy_button = self.get_first_smartphone_buy_button()
         self.move_to_element(first_smartphone_buy_button, self.driver)
@@ -151,15 +171,37 @@ class Smartphones_page(Base):
         self.load_page()
         self.get_current_url()
         self.check_url(self.check_u)
+        self.click_review_checkbox_button()
         self.fill_min_price_field(MIN_PRICE_1)
         self.fill_max_price_field(MAX_PRICE_1)
-        self.click_reliable_models_checkbox_button()
         self.click_discounts_checkbox_button()
         self.click_xiaomi_checkbox_button()
-        self.click_review_checkbox_button()
+        self.click_reliable_models_checkbox_button()
         self.click_apply_filters_button()
         self.click_sorting_method_button()
         self.click_popular_checkbox_button()
+        time.sleep(2)
+        first_smartphone_name, first_smartphone_price = self.get_first_smartphone_name().text, self.get_first_smartphone_price().text
+        self.click_first_smartphone_buy_button()
+        self.click_to_cart_button()
+
+        return first_smartphone_name, first_smartphone_price
+    
+    def select_smartphone_2(self):
+        self.check_u = "https://www.dns-shop.ru/catalog/17a8a01d16404e77/smartfony/"
+
+        self.load_page()
+        self.get_current_url()
+        self.check_url(self.check_u)
+        self.click_review_checkbox_button()
+        self.fill_min_price_field(MIN_PRICE_1)
+        self.fill_max_price_field(MAX_PRICE_1)
+        self.click_discounts_checkbox_button()
+        self.click_realme_checkbox_button()
+        self.click_reliable_models_checkbox_button()
+        self.click_apply_filters_button()
+        self.click_sorting_method_button()
+        self.click_best_score_checkbox_button()
         time.sleep(2)
         first_smartphone_name, first_smartphone_price = self.get_first_smartphone_name().text, self.get_first_smartphone_price().text
         self.click_first_smartphone_buy_button()
--- tests/test_buy_smartphones.py
+++ tests/test_buy_smartphones.py
@@ -32,11 +32,36 @@ def test_buy_smartphone_1(set_group, set_up):
     smartphone_name, smartphone_price = smartphones_page.select_smartphone_1()
     smartphone_price = smartphone_price.replace(' ', '')[0:5]
     smartphone_name = smartphone_name[0:smartphone_name.find('[')].replace(' ', '')
-    # print()
-    # print(smartphone_name)
-    # print()
 
     cart_page = Cart_page(driver, smartphone_name, smartphone_price)
     cart_page.finish()
 
-    # driver.quit()
+    driver.quit()
+
+def test_buy_smartphone_2(set_group, set_up):
+    options = webdriver.ChromeOptions()
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--ignore-ssl-errors')
+    options.add_experimental_option("detach", True)
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    g = Service(CHROMEDRIVE_PATH)
+    driver = webdriver.Chrome(options=options, service=g)
+
+    main_page = Main_page(driver)
+    main_page.select_smartphones_phototechnics()
+
+    smartphones_phototechnics_page = Smartphones_phototechnics_page(driver)
+    smartphones_phototechnics_page.select_smartphones_gadgets()
+
+    smartphones_gadgets_page = Smartphones_gadgets_page(driver)
+    smartphones_gadgets_page.select_smartphones()
+
+    smartphones_page = Smartphones_page(driver)
+    smartphone_name, smartphone_price = smartphones_page.select_smartphone_2()
+    smartphone_price = smartphone_price.replace(' ', '')[0:5]
+    smartphone_name = smartphone_name[0:smartphone_name.find('[')].replace(' ', '')
+
+    cart_page = Cart_page(driver, smartphone_name, smartphone_price)
+    cart_page.finish()
+
+    driver.quit()
